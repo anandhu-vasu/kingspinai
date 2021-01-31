bc702648d988010c7e0f029d0e3e727ca80e61f7
Anandhu Vasu <anandhu.vasu101@gmail.com>
2021-01-31 09:58:22 +0530

    index page

---
+++ chatbot/static/js/custom.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $("#ach-counter").waypoint(
+        function (_ide) {
+            $(this).find(".counter-ls-value").each(function () {
+                $(this)
+                    .prop("Counter", 0)
+                    .animate(
+                        {
+                            Counter: $(this).text(),
+                        },
+                        {
+                            duration: 3500,
+                            easing: "swing",
+                            step: function (now) {
+                                $(this).text(Math.ceil(now));
+                            },
+                        }
+                    );
+            });
+            this.destroy();
+        },
+        { offset: "100%",continuous: false}
+    );
+});
