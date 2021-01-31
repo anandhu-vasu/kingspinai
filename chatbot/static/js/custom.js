f937e325ca96b0232eb9a3713dd8a549f2009404
SOORAJ SHAJI <soorajshaji6@gmail.com>
2021-01-31 06:21:33 +0530

    count update... number is not increasing

--- chatbot/static/js/custom.js
+++ chatbot/static/js/custom.js
@@ -21,4 +21,27 @@ $(document).ready(function () {
         },
         { offset: "100%",continuous: false}
     );
+
+    $("#count").waypoint(
+        function (_ide) {
+            $(this).find(".counter-ls-value").each(function () {
+                $(this)
+                    .prop("Counter", 0)
+                    .animate(
+                        {
+                            Counter: $(this).text(),
+                        },
+                        {
+                            duration: 3500,
+                            easing: "swing",
+                            step: function (now) {
+                                $(this).text(Math.ceil(now));
+                            },
+                        }
+                    );
+            });
+            this.destroy();
+        },
+        { offset: "100%",continuous: false}
+    );
 });
