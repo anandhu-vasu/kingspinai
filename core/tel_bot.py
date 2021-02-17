6b81e51d57a9aafdd849853e3be896a0c196c3c8
Anandhu Vasu <anandhu.vasu101@gmail.com>
2021-02-17 13:42:07 +0530

    import fix

--- core/tel_bot.py
+++ core/tel_bot.py
@@ -2,7 +2,7 @@ import logging
 import os
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-from bots import KingspinAI
+from core.bots import KingspinAI
 
 # Enable logging
 logging.basicConfig(
