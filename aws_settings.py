5335c78383b5ed742a95fc534867955ccdeaa76c
Anandhu Vasu <anandhu.vasu101@gmail.com>
2021-05-09 00:33:30 +0530

    RDS Setup

--- aws_settings.py
+++ aws_settings.py
@@ -1,4 +1,18 @@
+from pathlib import Path
 import os
+import subprocess
+import ast
+
+
+def get_environ_vars():
+    completed_process = subprocess.run(
+        ['/opt/elasticbeanstalk/bin/get-config', 'environment'],
+        stdout=subprocess.PIPE,
+        text=True,
+        check=True
+    )
+
+    return ast.literal_eval(completed_process.stdout)
 
 if 'RDS_HOSTNAME' in os.environ:
     DATABASES = {
@@ -11,4 +25,15 @@ if 'RDS_HOSTNAME' in os.environ:
             'PORT': os.environ['RDS_PORT'],
         }
     }
-    
+ else:
+     env_vars = get_environ_vars()
+     DATABASES = {
+         'default': {
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': env_vars['RDS_DB_NAME'],
+         'USER': env_vars['RDS_USERNAME'],
+         'PASSWORD': env_vars['RDS_PASSWORD'],
+         'HOST': env_vars['RDS_HOSTNAME'],
+         'PORT': env_vars['RDS_PORT'],
+        }
+    }
